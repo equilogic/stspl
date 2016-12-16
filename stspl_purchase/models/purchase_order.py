a6697696c15491cc4ba5f5795cab3150f86fa6a5
Yogendrasinh Chavda <y.chavda.serpentcs@gmail.com>
2016-12-16 19:08:09 +0530

    [ADD/IMP] stspl_account, stspl_purchase : Override fields view get method to change report name.

--- stspl_purchase/models/purchase_order.py
+++ stspl_purchase/models/purchase_order.py
@@ -39,7 +39,32 @@ class purchase_order(models.Model):
         res = super(purchase_order,self).create(vals)
         return res
         
-    
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+        '''
+        Override this method to Rename Report name to print from Quatation and Purchase order menu.
+        '''
+        action_obj = self.env['ir.actions.actions']
+        result = super(purchase_order, self).fields_view_get(view_id, 
+            view_type, toolbar=toolbar, submenu=submenu)
+        cr,uid,context = self.env.args
+        if toolbar and context.get('params', False) and context['params'].get('action', False):
+            action_rec = action_obj.browse(context['params']['action'])
+            if result.get('toolbar', False) and result['toolbar'].get('print', False):
+                for report_dict in result['toolbar']['print']:
+                    if action_rec.name == 'Requests for Quotation' and report_dict.get('report_name',False) and \
+                                        report_dict['report_name'] == 'purchase.report_purchaseorder':
+                        report_dict.update({'display_name': 'Quotation Order','name': 'Quotation Order',
+                                            'string': 'Quotation Order'})
+                    if action_rec.name == 'Requests for Quotation' and report_dict.get('report_name',False) and \
+                                        report_dict['report_name'] == 'stspl_purchase.report_purchase_order':
+                        report_dict.update({'display_name': 'Quotation Report','name': 'Quotation Report',
+                                            'string': 'Quotation Report'})
+                    if action_rec.name == 'Purchase Orders' and report_dict.get('report_name',False) and \
+                                        report_dict['report_name'] == 'purchase.report_purchasequotation':
+                        report_dict.update({'display_name': 'Request for Purchase','name': 'Request for Purchase',
+                                            'string': 'Request for Purchase'})
+        return result
     
 
 
